32d68803e5583edc2f3a62d6128b1ec053b126c1
Mailon Torres <mailontorres@gmail.com>
2026-05-14 02:43:32 -0300

    feat: implement responsive sidebar component with provider, trigger, and mobile support

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,18 +2,19 @@ import type { AnyFieldApi } from "@tanstack/react-form";
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+// biome-ignore lint/performance/noBarrelFile: export cva
+export { cva, type VariantProps } from "class-variance-authority";
+
 export function cn(...inputs: ClassValue[]) {
-    return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs));
 }
 
 export function formFieldParameters(field: AnyFieldApi, source: any = {}) {
-    return Object.assign(
-        {
-            name: field.name,
-            onBlur: field.handleBlur,
-            onChange: (e: any) => field.handleChange(e.target.value),
-            value: field.state.value,
-        },
-        source,
-    );
+  return {
+    name: field.name,
+    onBlur: field.handleBlur,
+    onChange: (e: any) => field.handleChange(e.target.value),
+    value: field.state.value,
+    ...source,
+  };
 }
