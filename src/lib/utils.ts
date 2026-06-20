76979890aee47faed33fbe04f41392b660a7f67b
Mailon Torres <mailontorres@gmail.com>
2026-06-20 16:31:44 -0300

    feat(utils): adicionar id ao retorno de formFieldParameters

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,15 +6,16 @@ import { twMerge } from "tailwind-merge";
 export { cva, type VariantProps } from "class-variance-authority";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+    return twMerge(clsx(inputs));
 }
 
 export function formFieldParameters(field: AnyFieldApi, source: any = {}) {
-  return {
-    name: field.name,
-    onBlur: field.handleBlur,
-    onChange: (e: any) => field.handleChange(e.target.value),
-    value: field.state.value,
-    ...source,
-  };
+    return {
+        id: field.name,
+        name: field.name,
+        onBlur: field.handleBlur,
+        onChange: (e: any) => field.handleChange(e.target.value),
+        value: field.state.value,
+        ...source,
+    };
 }
